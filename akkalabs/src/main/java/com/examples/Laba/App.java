faaf878a9535804b907acdd8bf9a8c5466025731
Egorychev Artem <temych_2000@mail.ru>
2019-11-07 02:04:13 +0300

    Scripted auto-commit on change (2019-11-07 02:04:13) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/App.java
+++ akkalabs/src/main/java/com/examples/Laba/App.java
@@ -10,14 +10,15 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("routers");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        
+
         MainHttp instance = new MainHttp(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.createRoute(system).flow(system, materializer);
