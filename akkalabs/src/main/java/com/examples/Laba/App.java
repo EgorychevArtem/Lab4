ebe8b2c759ee7fc35b9f730487b4164d1815b64d
Egorychev Artem <temych_2000@mail.ru>
2019-11-07 02:00:52 +0300

    Scripted auto-commit on change (2019-11-07 02:00:52) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/App.java
+++ akkalabs/src/main/java/com/examples/Laba/App.java
@@ -1,7 +1,12 @@
 package com.examples.Laba;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+
 public class App {
     public static void main(String[] args){
+        ActorSystem system = ActorSystem.create("routers");
+        final Http http = Http.get(system);
         
     }
 }
