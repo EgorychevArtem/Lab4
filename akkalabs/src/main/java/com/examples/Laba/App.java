16fe937e8cdb92adab91f45983f3328d8eddc2be
Egorychev Artem <temych_2000@mail.ru>
2019-11-07 02:06:21 +0300

    Scripted auto-commit on change (2019-11-07 02:06:21) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/App.java
+++ akkalabs/src/main/java/com/examples/Laba/App.java
@@ -19,9 +19,10 @@ public class App {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        MainHttp instance = new MainHttp(system);
+        //MainHttp instance = new MainHttp(system);
+        App app = new App();
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-                instance.createRoute(system).flow(system, materializer);
+                app.createRoute(system).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
