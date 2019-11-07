dec332aaa753b82db8bf4c1ba4723b916c9eac02
Egorychev Artem <temych_2000@mail.ru>
2019-11-08 00:06:13 +0300

    Scripted auto-commit on change (2019-11-08 00:06:13) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/ActorPerform.java
+++ akkalabs/src/main/java/com/examples/Laba/ActorPerform.java
@@ -2,7 +2,23 @@ package com.examples.Laba;
 
 import akka.actor.ActorRef;
 
-public class ActorPerform {
-ActorRef storage;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
+public class ActorPerform {
+    ActorRef storage;
+
+    ActorPerform(ActorRef storage) {
+        this.storage = storage;
+    }
+
+    String checkTest(String script, String NameFunction, String[] args) throws ScriptException, NoSuchMethodException {
+        ScriptEngine engine = new
+                ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(script);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(NameFunction, args).toString();
+    }
 }
