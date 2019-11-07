fd5dd0d30aca0fc7996e63e6b6e7094c99d42968
Egorychev Artem <temych_2000@mail.ru>
2019-11-08 00:14:12 +0300

    Scripted auto-commit on change (2019-11-08 00:14:12) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/ActorPerform.java
+++ akkalabs/src/main/java/com/examples/Laba/ActorPerform.java
@@ -1,5 +1,6 @@
 package com.examples.Laba;
 
+import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 
 import javax.script.Invocable;
@@ -7,7 +8,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-public class ActorPerform {
+public class ActorPerform extends AbstractActor{
     ActorRef storage;
 
     ActorPerform(ActorRef storage) {
@@ -20,5 +21,21 @@ public class ActorPerform {
         engine.eval(script);
         Invocable invocable = (Invocable) engine;
         return invocable.invokeFunction(NameFunction, args).toString();
+    }
+
+    public AbstractActor.Receive createReceive() {
+        return receiveBuilder()
+                .match(InputTestMessage.class, m-> {
+                    boolean resflag;
+                    String description;
+                    String out = checkTest(m.script, m.NameFunction, m.args);
+                    if (out == m.res){
+                        resflag = true;
+                    } else{
+                        resflag = false;
+                    }
+                    description = resflag ? "ok" : "Expected: " + m.res + "Out: " + out;
+                    this.storage.tell(new InputResMessage(m.pkg, new Test(m.name, resflag, description)),getSelf());
+                }).build();
     }
 }
