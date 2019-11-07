5320bcb1663505c856dcd686e7a9a9914d10330a
Egorychev Artem <temych_2000@mail.ru>
2019-11-07 23:39:46 +0300

    Scripted auto-commit on change (2019-11-07 23:39:46) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/InputResMessage.java
+++ akkalabs/src/main/java/com/examples/Laba/InputResMessage.java
@@ -1,5 +1,11 @@
 package com.examples.Laba;
 
 public class InputResMessage {
-    
+    String pkg;
+    Test test;
+    InputResMessage(String pkg, Test test){
+        this.pkg = pkg;
+        this.test = test;
+    }
+
 }
