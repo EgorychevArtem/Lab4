afd96f019c36aa96305b4bd956c420a6c6fee5f0
Egorychev Artem <temych_2000@mail.ru>
2019-11-08 00:15:53 +0300

    Scripted auto-commit on change (2019-11-08 00:15:53) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/Test.java
+++ akkalabs/src/main/java/com/examples/Laba/Test.java
@@ -1,6 +1,11 @@
 package com.examples.Laba;
 
 public class Test {
+    String name, description;
+    Boolean resflag;
     public Test(String name, boolean resflag, String description) {
+        this.name = name;
+        this.resflag = resflag;
+        this.description = description;
     }
 }
