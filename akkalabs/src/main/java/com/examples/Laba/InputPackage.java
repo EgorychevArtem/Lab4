62a55973eb6a3acb011779e17ce6040a70a59733
Egorychev Artem <temych_2000@mail.ru>
2019-11-09 01:23:50 +0300

    Scripted auto-commit on change (2019-11-09 01:23:50) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/InputPackage.java
+++ akkalabs/src/main/java/com/examples/Laba/InputPackage.java
@@ -8,5 +8,11 @@ public class InputPackage {
     String packageId, script, Namefunction;
     List<InputTest> tests;
 
-    InputPackage(@JsonProperty("testName") String testName)
+    InputPackage(@JsonProperty("packageId") String packageId, @JsonProperty("script") String script,
+                 @JsonProperty("Namefunction") String Namefunction, @JsonProperty("tests") List<InputTest> tests){
+        this.packageId = packageId;
+        this.script = script;
+        this.Namefunction = Namefunction;
+        this.tests = tests;
+    }
 }
