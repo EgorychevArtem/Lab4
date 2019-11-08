0c3c40b04b1db7af36f786e049691428418d1f6f
Egorychev Artem <temych_2000@mail.ru>
2019-11-09 01:31:32 +0300

    Scripted auto-commit on change (2019-11-09 01:31:32) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/ActorRouter.java
+++ akkalabs/src/main/java/com/examples/Laba/ActorRouter.java
@@ -2,17 +2,21 @@ package com.examples.Laba;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.routing.RoundRobinPool;
 
 public class ActorRouter extends AbstractActor {
     ActorRef storage, router;
 
     ActorRouter(){
-        this.storage = getCo
+        this.storage = getContext().actorOf(Props.create(ActorResult.class));
+        this.router = getContext().actorOf(Props.create(ActorPerform.class, this.storage)
+        .withRouter(new RoundRobinPool()));
     }
 
     @Override
     public Receive createReceive() {
         return null;
     }
-    
+
 }
