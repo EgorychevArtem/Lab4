e592be8c1c3a38ec5252948be065b5e912aa8bf6
Egorychev Artem <temych_2000@mail.ru>
2019-11-09 02:32:59 +0300

    Scripted auto-commit on change (2019-11-09 02:32:59) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/App.java
+++ akkalabs/src/main/java/com/examples/Laba/App.java
@@ -7,15 +7,19 @@ import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
+import java.util.regex.Pattern;
 
 import static akka.http.javadsl.server.Directives.path;
 import static akka.http.javadsl.server.Directives.route;
@@ -43,20 +47,21 @@ public class App extends AllDirectives {
                 .thenAccept(unbound -> system.terminate());
     }
 
-    private Route createRoute(ActorSystem system) {
+    Route createRoute(ActorSystem system) {
         route(
-                path("result", () ->
-                            route(
-                                    get( () -> {
-                                        return parameter("packageId", pkg ->{
-                                            
-                                        })
-                                            }
+                path("result", () -> {
+                    return route(
+                            get(() -> {
+                                return parameter("packageId", pkg -> {
+                                    Future<Result> res = Patterns.ask(
+                                            router, new OutputRes(pkg)
+                                    ).map(r -> r, system.dispatcher());
+                                    return completeOKWithFuture(res, Jackson.marshaller());
+                                });
+                            })
+                    );
 
-                                    )
-                            )
-
-                )
+                })
         )
     }
 }
