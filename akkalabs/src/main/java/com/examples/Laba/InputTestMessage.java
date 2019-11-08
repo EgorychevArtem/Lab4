cb09cd0990245b972392edaeec7a777457e82643
Egorychev Artem <temych_2000@mail.ru>
2019-11-09 01:58:53 +0300

    Scripted auto-commit on change (2019-11-09 01:58:52) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/InputTestMessage.java
+++ akkalabs/src/main/java/com/examples/Laba/InputTestMessage.java
@@ -2,9 +2,9 @@ package com.examples.Laba;
 
 public class InputTestMessage {
     String pkg, name, script, NameFunction, res;
-    String[] args;
+    Object[] args;
 
-    InputTestMessage(String pkg, String name, String script, String NameFunction, String res, String[] args){
+    InputTestMessage(String pkg, String name, String script, String NameFunction, String res, Object[] args){
         this.pkg = pkg;
         this.name = name;
         this.script = script;
