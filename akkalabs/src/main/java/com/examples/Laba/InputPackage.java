adab965fc0c7bbe5977d18a03289b89881c9a2f5
Egorychev Artem <temych_2000@mail.ru>
2019-11-09 01:20:24 +0300

    Scripted auto-commit on change (2019-11-09 01:20:24) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/InputPackage.java
+++ akkalabs/src/main/java/com/examples/Laba/InputPackage.java
@@ -1,4 +1,12 @@
 package com.examples.Laba;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class InputPackage {
+    String packageId, script, Namefunction;
+    List<InputTest> tests;
+
+    InputPackage(@JsonProperty("testName") String testName)
 }
