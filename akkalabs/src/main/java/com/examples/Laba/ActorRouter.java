01de50b8c0441866ccf375dcb1c05d2a0c381e18
Egorychev Artem <temych_2000@mail.ru>
2019-11-09 01:19:02 +0300

    Scripted auto-commit on change (2019-11-09 01:19:02) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/ActorRouter.java
+++ akkalabs/src/main/java/com/examples/Laba/ActorRouter.java
@@ -1,5 +1,5 @@
 package com.examples.Laba;
 
 public class ActorRouter {
-    
+
 }
--- akkalabs/src/main/java/com/examples/Laba/InputTest.java
+++ akkalabs/src/main/java/com/examples/Laba/InputTest.java
@@ -1,4 +1,17 @@
 package com.examples.Laba;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class InputTest {
+    String testName, result;
+    List<String> args;
+
+    InputTest(@JsonProperty("testName") String testName, @JsonProperty("result") String result,
+              @JsonProperty("args") List<String> args){
+        this.testName = testName;
+        this.result = result;
+        this.args = args;
+    }
 }
