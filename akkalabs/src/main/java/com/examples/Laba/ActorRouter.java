22bcb19f38eed290b2f45ea2f0e1873a00288ee7
Egorychev Artem <temych_2000@mail.ru>
2019-11-09 01:28:57 +0300

    Scripted auto-commit on change (2019-11-09 01:28:57) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/ActorRouter.java
+++ akkalabs/src/main/java/com/examples/Laba/ActorRouter.java
@@ -1,8 +1,17 @@
 package com.examples.Laba;
 
+import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 
-public class ActorRouter {
+public class ActorRouter extends AbstractActor {
     ActorRef storage, router;
-    ActorRouter()
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+
+    ActorRouter(){
+        this.storage = getCo
+    }
 }
