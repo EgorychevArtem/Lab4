4c4a660a5c645ae037339e252b444edbcc6e1e6c
Egorychev Artem <temych_2000@mail.ru>
2019-11-22 22:17:39 +0300

    Scripted auto-commit on change (2019-11-22 22:17:39) by gitwatch.sh

--- akkalabs/src/main/java/com/examples/Laba/InputTest.java
+++ akkalabs/src/main/java/com/examples/Laba/InputTest.java
@@ -1,55 +1,8 @@
 package com.examples.Laba;
 
-/*import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
-public class InputTest {
-    String testName, expectedResult;
-    Object[] params;
-
-    @JsonCreator
-    InputTest(@JsonProperty("testName") String testName, @JsonProperty("expectedResult") String expectedResult,
-              @JsonProperty("params") Object[] params){
-        this.testName = testName;
-        this.expectedResult = expectedResult;
-        this.params = params;
-    }
-}
-*/
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/*public class InputTest {
-    String testName;
-    String expectedResult;
-    Object[] params;
-
-    @JsonCreator
-    public InputTest(
-            @JsonProperty("testName") String testName,
-            @JsonProperty("expectedResult") String expectedResult,
-            @JsonProperty("params") Object[] params
-    ) {
-        this.testName = testName;
-        this.expectedResult = expectedResult;
-        this.params = params;
-    }
-
-    public String getTestName(){
-        return testName;
-    }
-
-    public String getExpectedResult(){
-        return expectedResult;
-    }
-
-    public Object[] getParams(){
-        return params;
-    }
-}*/
 public class InputTest {
     String testName;
     String expectedResult;
